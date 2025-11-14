2713e6933ef4e34f8fbd7dd24498a9361304736a
Tom Honermann <tom.honermann@intel.com>
2025-11-14 14:02:15 -0800

    Misc cleanup.
    - Added/edited lots of comments.
    - Expanded testing for sycl_kernel_launch lookup and reorganized the tests
      to make it easier to identify and/or plug testing gaps.
    - Added a missing sycl_kernel_entry_point attribute in a test.
    - Reordered various declarations for better grouping and consistency.
    - Renamed some variables and functions.
    - Removed an unnecessary include directive.
    - Adjusted the location used for the implicit sycl_kernel_launch call. The
      opening brace of the original function body is used where available and
      the general function location is used otherwise.
    - Corrected lookup to unconditionally look for a template name; previously
      a spurious error about a 'template' keyword could be issued.
    - Added missing code gen checks for use of a member function as the SYCL
      kernel entry point function.
    - Other misc style edits for consistency.

--- clang/lib/Sema/TreeTransform.h
+++ clang/lib/Sema/TreeTransform.h
@@ -13026,7 +13026,6 @@ StmtResult TreeTransform<Derived>::TransformUnresolvedSYCLKernelCallStmt(
     return StmtError();
 
   ExprResult IdExpr = getDerived().TransformExpr(S->getKernelLaunchIdExpr());
-
   if (IdExpr.isInvalid())
      return StmtError();
 
