4dd78ee7dd9f58122a6057b84b58438d87c8624c
Podchishchaeva, Mariya <mariya.podchishchaeva@intel.com>
2026-07-14 03:53:38 -0700

    Fix checks in pch test

--- clang/test/SemaSYCL/sycl-kernel-entry-point-attr-kernel-name-pch.cpp
+++ clang/test/SemaSYCL/sycl-kernel-entry-point-attr-kernel-name-pch.cpp
@@ -33,11 +33,11 @@ template void pch_test2<KN<2>>();
 
 #--- test.cpp
 // expected-error@+3 {{the 'clang::sycl_kernel_entry_point' kernel name argument conflicts with a previous declaration}}
-// expected-note@pch.h:8 {{previous declaration is here}}
+// expected-note@pch.h:11 {{previous declaration is here}}
 [[clang::sycl_kernel_entry_point(KN<1>)]]
 void test1() {}
 
 // expected-error@+3 {{the 'clang::sycl_kernel_entry_point' kernel name argument conflicts with a previous declaration}}
-// expected-note@pch.h:12 {{previous declaration is here}}
+// expected-note@pch.h:15 {{previous declaration is here}}
 [[clang::sycl_kernel_entry_point(KN<2>)]]
 void test2() {}
